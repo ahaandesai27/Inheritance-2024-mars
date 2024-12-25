204caa4472e458f39f0dc9e126a2b75e8d803cb3
Raunak-2005 <raunakthanawala@gmail.com>
2024-12-26 01:24:31 +0530

    Made Files for most pages neeeded

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,11 +1,31 @@
+import 'package:app/pages/loginpage.dart';
+import 'package:app/pages/recipepage.dart';
+import 'package:app/pages/signuppage.dart';
+import 'package:app/pages/user.dart';
+import 'package:app/pages/vegetable.dart';
 import 'package:flutter/material.dart';
-import 'pages/user.dart';
+
+void main() {
+  runApp(const Recipaura());
+}
 
 class Recipaura extends StatelessWidget {
   const Recipaura({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return UserPage();
+    return MaterialApp(
+        title: 'Recipaura',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const UserPage(),
+        routes: {
+          "/": (context) => const UserPage(),
+          "/signup": (context) => const SignUpPage(),
+          "/login": (context) => const LoginPage(),
+          "/grocery": (context) => const GroceryPage(),
+          '/recipe': (context) => const RecipePage(),
+        });
   }
 }

879bb3ec73dc634f34a177e189523ba3bc65c8c6
Raunak-2005 <raunakthanawala@gmail.com>
2025-01-01 22:23:27 +0530

    Routing

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:app/pages/recipepage.dart';
 import 'package:app/pages/signuppage.dart';
 import 'package:app/pages/user.dart';
 import 'package:app/pages/vegetable.dart';
+import 'package:app/utils/routes.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -19,14 +20,13 @@ class Recipaura extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: const LoginPage(),
         routes: {
-         // "/": (context) => const UserPage(),
-         // "/signup": (context) => const SignUpPage(),
-         //"/login": (context) => const LoginPage(),
-         // "/grocery": (context) => const GroceryPage(),
-         // '/recipe': (context) => const RecipePage(),
-        }
-      );
+          "/": (context) => const SignUpPage(),
+          MyRoutes.userRoute: (context) => const UserPage(),
+          MyRoutes.signUpRoute: (context) => const SignUpPage(),
+          MyRoutes.loginRoute: (context) => const LoginPage(),
+          MyRoutes.vegetableRoute: (context) => const GroceryPage(),
+          MyRoutes.recipeRoute: (context) => const RecipePage(),
+        });
   }
 }
---
+++ app/lib/utils/routes.dart
@@ -0,0 +1,7 @@
+class MyRoutes {
+  static String loginRoute = "/login";
+  static String userRoute = "/user";
+  static String signUpRoute = "/signup";
+  static String vegetableRoute = "/grocery";
+  static String recipeRoute = "/recipe";
+}
